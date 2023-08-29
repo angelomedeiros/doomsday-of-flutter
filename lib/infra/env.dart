4f486b4c3d98cc78f9b171cf46a92405d0306920
Angelo Medeiros <angelomedeiros3@gmail.com>
2023-08-29 20:33:19 -0300

    feat: config enviroment variables

---
+++ lib/infra/env.dart
@@ -0,0 +1,21 @@
+import 'package:envied/envied.dart';
+
+part 'env/env.g.dart';
+
+@Envied(path: '.env')
+abstract class Env {
+  @EnviedField(varName: 'API_KEY', obfuscate: true)
+  static final String apiKey = _Env.apiKey;
+  @EnviedField(varName: 'APP_ID')
+  static const String appId = _Env.appId;
+  @EnviedField(varName: 'MESSAGING_SENDER_ID')
+  static const String messagingSenderId = _Env.messagingSenderId;
+  @EnviedField(varName: 'PROJECT_ID')
+  static const String projectId = _Env.projectId;
+  @EnviedField(varName: 'AUTH_DOMAIN')
+  static const String authDomain = _Env.authDomain;
+  @EnviedField(varName: 'STORAGE_BUCKET')
+  static const String storageBucket = _Env.storageBucket;
+  @EnviedField(varName: 'MEASUREMENT_ID')
+  static const String measurementId = _Env.measurementId;
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,19 @@
+import 'package:doomsday_of_flutter/infra/env.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'presentation/presentation.dart';
 
 void main() async {
-  await dotenv.load();
-
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: FirebaseOptions(
-      apiKey: dotenv.get('API_KEY'),
-      appId: dotenv.get('APP_ID'),
-      messagingSenderId: dotenv.get('MESSAGING_SENDER_ID'),
-      projectId: dotenv.get('PROJECT_ID'),
-      authDomain: dotenv.get('AUTH_DOMAIN'),
-      storageBucket: dotenv.get('STORAGE_BUCKET'),
-      measurementId: dotenv.get('MEASUREMENT_ID'),
+      apiKey: Env.apiKey,
+      appId: Env.appId,
+      messagingSenderId: Env.messagingSenderId,
+      projectId: Env.projectId,
+      authDomain: Env.authDomain,
+      storageBucket: Env.storageBucket,
+      measurementId: Env.measurementId,
     ),
   );
 
