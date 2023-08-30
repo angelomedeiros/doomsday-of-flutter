6b22a7e5970464be8db6a5e23f92b6044e8e34ce
Angelo Medeiros <angelomedeiros3@gmail.com>
2023-08-29 21:08:33 -0300

    fix: audio path

--- lib/presentation/screens/home.dart
+++ lib/presentation/screens/home.dart
@@ -58,7 +58,7 @@ class _HomeScreenState extends State<HomeScreen>
   void _initPlayer() async {
     try {
       await player?.setAsset(
-        'audios/background.mp3',
+        'assets/audios/background.mp3',
         initialPosition: const Duration(milliseconds: 23600),
       );
       await player?.setLoopMode(LoopMode.all);
